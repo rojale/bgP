b930cf6574209c130d1441005e471073d4686d84
rojale <j08zhang24@gmail.com>
2017-05-22 16:27:42 -0400

    initial msg

---
+++ src/pages/add-item/add-item.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import {NavController, ViewController } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-add-item',
+  templateUrl: 'add-item.html',
+})
+
+
+export class AddItemPage {
+	title;
+	minPlayers;
+	maxPlayers;
+  gameName;
+  public games = [];
+  httpRequest: XMLHttpRequest;
+
+
+  constructor(public navCtrl: NavController, public view: ViewController) {
+    this.httpRequest = new XMLHttpRequest();
+
+  }
+
+  
+  close(){
+  	this.view.dismiss();
+  }
+
+  
+
+  nameSearch(){
+    var gName = this.gameName;
+    var searchurl = 'http://192.168.2.50/JS/bggcall.php?gname=' + gName;
+    this.bgRequest(searchurl);
+  }
+
+  bgRequest(url) {
+  if (!this.httpRequest) {
+    console.log("httpRequest instance could not be created");
+  }
+
+
+  this.httpRequest.open('GET', url, true);
+  this.httpRequest.onreadystatechange = () => {
+    if (this.httpRequest.readyState ==XMLHttpRequest.DONE) {
+      var bgInfo = JSON.parse(this.httpRequest.response);
+
+      for (let i in bgInfo) {
+        this.saveGame({name: bgInfo[i][0], year: bgInfo[i][1], minPlayers: bgInfo[i][2], maxPlayers: bgInfo[i][3]});
+      }
+
+
+    }
+  }
+  this.httpRequest.send()
+
+  }
+
+  saveGame(game){
+    this.games.push(game);
+  }
+
+  addGame(i){
+    let newItem = {
+      name: this.games[i].name,
+      minPlayers: this.games[i].minPlayers,
+      maxPlayers: this.games[i].maxPlayers
+    };
+    this.view.dismiss(newItem);
+  }
+
+}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { NavController, ModalController } from 'ionic-angular';
+import { AddItemPage } from '../add-item/add-item'
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+	public items=[];
+
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
+
+  }
+
+  ionViewDidLoad(){
+
+  }
+
+  addItem(){
+
+    let addModal = this.modalCtrl.create(AddItemPage);
+
+    addModal.onDidDismiss((item) =>{
+      if(item) {
+        this.saveItem(item);
+      }
+    });
+
+    addModal.present();
+
+  }
+
+  saveItem(item){
+    this.items.push(item);
+
+  }
+
+  viewItem(){
+
+  }
+
+}
